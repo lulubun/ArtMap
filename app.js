022f48ad2b359ad7d3666dc036561f3a458d0c15
lulubun <laurapetitt@gmail.com>
2016-12-05 22:05:51 -0500

    restarted code from Google API documentation

--- app.js
+++ app.js
@@ -1,72 +1,50 @@
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
-
-var xhr = createCORSRequest('GET', url);
-if (!xhr) {
-  throw new Error('CORS not supported');
-}
-
 function getLocation() {
   $(".js-start").on("click", "button", function(event) {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPlaces, getMap);
+        navigator.geolocation.getCurrentPosition(initMap, callback);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
       }  
   });
 };
 
-function getPlaces(position) {
-  var long = position.coords.longitude;
-  var latt = position.coords.latitude;
-  var BASE_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
-  var mapData = {
-    key: 'AIzaSyDgk52fKecH4YpjA3U7y9YlMvI-QlOtEXg',
-    location: latt + ',' + long,
-    radius: 16000,
-    type: 'art_gallery'
+var map;
+var service;
+var infowindow;
+
+function initMap(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: latitude,
+        lng: longitude
+      },
+      zoom: 15
+    });
+
+  var request = {
+    location: {
+      lat:latitude,
+      lng: longitude
+    },
+    radius: '16000',
+    types: ['art_gallery']
   };
-  $.getJSON(BASE_URL, mapData, displayPins);
-};
 
-function getMap(position) {
-  var long = position.coords.longitude;
-  var latt = position.coords.latitude;
-  var myLatLng = latt, long;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng
-  });
-};
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
 
-function displayPins(data) {
-  var marker = new google.maps.Marker({
-    position: {lat: results.geometry.location.lat, lng: results.geometry.location.lng},
-    map: map,
-    title: results.name,
-  });
-};
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
 
 $(function() {
   getLocation();
