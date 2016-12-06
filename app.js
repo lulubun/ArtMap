5b67d4ab3877d7de8f088af690d76096a696cd97
lulubun <laurapetitt@gmail.com>
2016-12-05 23:15:37 -0500

    merged two google code samples, map is showing up but search and markers are not at this point. button is being pushed but nothing is happening.

--- app.js
+++ app.js
@@ -1,51 +1,76 @@
-function getLocation() {
-  $(".js-start").on("click", "button", function(event) {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(initMap, callback);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-      }  
-  });
-};
-
 var map;
-var service;
-var infowindow;
+var pos;
 
-function initMap(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-
+function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        lat: latitude,
-        lng: longitude
-      },
-      zoom: 15
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
-
-  var request = {
-    location: {
-      lat:latitude,
-      lng: longitude
-    },
-    radius: '16000',
-    types: ['art_gallery']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+
+$(".js-start").on("click", "button", function(event) {
+  function findGalleries() {
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pos,
+      radius: 15000,
+      type: ['art_gallery']
+    }, callback);
+  };
+  
+
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      var place = results[i];
       createMarker(results[i]);
     }
   }
 }
 
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+});
+
+
 $(function() {
-  getLocation();
+  initMap();
 });
